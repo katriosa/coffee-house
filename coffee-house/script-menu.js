a339360350471df63e8987d196b856b9ccfca008
Catherine Chistyakova <ee.chistyakova@gmail.com>
2023-12-26 22:19:19 +0100

    refactor: displayCardsHandler in menu page

--- coffee-house/script-menu.js
+++ coffee-house/script-menu.js
@@ -46,21 +46,21 @@ toggleBurgerMenu();
 
 
 //Tabs
-const cardsMenu = () => {
+const displayCardsHandler = () => {
   const cardsContainer = document.querySelector('.cards-container');
-  const btnUpdate = document.querySelector('.load-more-btn');
+  const loadMoreBtn = document.querySelector('.load-more-btn');
   const tabsContainer = document.querySelector('.tab-navigation')
   const tabs = document.querySelectorAll('.tab-category')
+  let curNameCategory = 'coffee'
 
-
-  const displayCards = (cards, clickedBtnUpdate = false) => {
-    if (!clickedBtnUpdate) {
+  const displayCards = (cards, clickedloadMoreBtn = false) => {
+    if (!clickedloadMoreBtn) {
       cardsContainer.innerHTML = ''
     }
-    const startIndex = clickedBtnUpdate ? 5 : 1;
+    const startIndex = clickedloadMoreBtn ? 5 : 1;
     cards.forEach((card, i) => {
       cardsContainer.insertAdjacentHTML('beforeend',
-      `<div class="card-item">
+        `<div class="card-item">
         <div class="image-card-container">
           <img
             class="image-card"
@@ -78,214 +78,220 @@ const cardsMenu = () => {
     })
   }
 
-    
-  const createCards = (categoryArr) => {
-    const mediaQuery = window.matchMedia('(max-width: 768px)');
+  ////////
+  const mediaQuery = window.matchMedia('(max-width: 768px)');
+  let allCathegoryCards;
 
-    const getCardsToDisplay = () => {
-      if (mediaQuery.matches && categoryArr.length > 4) {
-        btnUpdate.style.display = 'flex';
-        return categoryArr.slice(0, 4);
-      } else {
-        btnUpdate.style.display = 'none';
-        return categoryArr;
-      }
+  const loadMoreButtonDisplay = (shouldDisplay) => {
+    loadMoreBtn.style.display = shouldDisplay ? 'flex' : 'none';
+  };
+  
+  const getCardsToDisplay = (mediaQuery) => {
+    if (mediaQuery.matches && allCathegoryCards.length > 4) {
+      const firstPartCards = allCathegoryCards.slice(0, 4);
+      loadMoreButtonDisplay(true);
+      displayCards(firstPartCards);
+    } else {
+      loadMoreButtonDisplay(false);
+      displayCards(allCathegoryCards);
     }
-
-    const btnUpdateHandler = () => {
-      if (categoryArr.length > 4) {
-        const restCards = categoryArr.slice(4);
-        displayCards(restCards, true);
-       btnUpdate.removeEventListener('click',btnUpdateHandler);
-
-      }
-      btnUpdate.style.display = 'none';
-    }  
-     
-    btnUpdate.addEventListener('click', btnUpdateHandler);
-    mediaQuery.addEventListener("change", getCardsToDisplay);
-    
-    const cardsToDisplay = getCardsToDisplay();
-    displayCards(cardsToDisplay)
-
+  }
+  
+  //////////
+  const getFullCathegoryArr = () => {
+    const fullCathegoryArr = products.filter((prod) => prod.category === curNameCategory);
+    if (fullCathegoryArr) {
+      allCathegoryCards = fullCathegoryArr;
+      getCardsToDisplay(mediaQuery);
+    };
   }
 
+  mediaQuery.addEventListener("change", getCardsToDisplay);
 
+  /////
   const selectTabHandler = (e) => {
     const selectedTab = e.target.closest('.tab-item');
     if (!selectedTab) return;
     tabs.forEach((tab) => tab.classList.remove('active'))
-      selectedTab.classList.add('active');
+    selectedTab.classList.add('active');
 
-    const curCategory = filterData(selectedTab.dataset.tab)
-    if (curCategory) {
-      createCards(curCategory)
-    }  
+    curNameCategory = selectedTab.dataset.tab;
+    getFullCathegoryArr();
   }
 
-  tabsContainer.addEventListener('click', selectTabHandler)
+  tabsContainer.addEventListener('click', selectTabHandler);
 
+///////////
+const loadMoreBtnHandler = () => { 
+        const restCards = allCathegoryCards.slice(4);
+        displayCards(restCards, true);
+        loadMoreButtonDisplay(false)
+      }
+     
+  loadMoreBtn.addEventListener('click', loadMoreBtnHandler);
 
-  const filterData = (curNameCategory = 'coffee') => {
-    const curCategory = products.filter((prod) => prod.category === curNameCategory);
-    return curCategory;
+  
+/////////
+  const displayInitialCards = () => {
+    getFullCathegoryArr();
   }
 
-  const initialCards = filterData();
-  createCards(initialCards);
-  }
-
-cardsMenu();
+  displayInitialCards();
+}
+displayCardsHandler();
 
 //Modal Window
-const cardsContainer = document.querySelector('.cards-container');
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const modalPriceContainer = document.querySelector('.modal-price-container');
-const btnCloseModal = document.querySelector('.btn-close-modal');
-const imageContainer = document.querySelector('.modal-image-container');
-const titleContainer = document.querySelector('.title-container');
-const tabsSize = document.querySelector('.tabs-size')
-const tabsAdditives = document.querySelector('.tabs-additives')
-let totalPrice= 0;
-let additivesPrice = 0;
+const toggleModalWindow =() => {
+  const cardsContainer = document.querySelector('.cards-container');
+  const modal = document.querySelector('.modal');
+  const overlay = document.querySelector('.overlay');
+  const modalPriceContainer = document.querySelector('.modal-price-container');
+  const btnCloseModal = document.querySelector('.btn-close-modal');
+  const imageContainer = document.querySelector('.modal-image-container');
+  const titleContainer = document.querySelector('.title-container');
+  const tabsSize = document.querySelector('.tabs-size')
+  const tabsAdditives = document.querySelector('.tabs-additives')
+  let totalPrice = 0;
+  let additivesPrice = 0;
 
-const chooseAdditives = (e) => {
-  const selected = e.target.closest('.tab-item');
-  if (!selected) return;
-
-  const isActive = selected.classList.toggle('active');
-
-  console.log(isActive);
-  const addPrice = +selected.dataset.tab;
-  if (isActive) {
-    totalPrice += addPrice;  
-    additivesPrice+=addPrice
-  } else {
-    totalPrice -= addPrice;
-    additivesPrice-=addPrice
-  }
-
-  createTotalPrice();
-}
-
-tabsAdditives.addEventListener('click', chooseAdditives);
-
-const createTotalPrice = ( ) => {
-  modalPriceContainer.innerHTML = '';
- 
-  modalPriceContainer.insertAdjacentHTML('beforeend',
-    `<h3 class="font-h3">Total:</h3>
-    <h3 class="font-h3">$${totalPrice.toFixed(2)}</h3>`
-  );
-}
-
- 
-
-const createSizeBlock = (obj) => {
-  const objKeys = Object.keys(obj.sizes);
-
-  objKeys.forEach((key) => {
-    const tabEl = document.createElement('button');
-    tabEl.innerHTML = `<span class="icon">${key.toUpperCase()}</span>${obj.sizes[key].size}`;
-    
-    tabEl.classList.add('tab-item');
-    tabEl.classList.add('font-link-and-button');
-    tabEl.setAttribute('data-tab', obj.sizes[key]['add-price']);
-
-    if (key === 's') {
-      tabEl.classList.add('active');
-    }
-    tabsSize.appendChild(tabEl)
-  })
-  totalPrice =+obj.price;
-  createTotalPrice();
-
-  const chooseSize = (e) => {
-    const allTabs = tabsSize.querySelectorAll('.tab-item');
+  const chooseAdditives = (e) => {
     const selected = e.target.closest('.tab-item');
     if (!selected) return;
 
-    allTabs.forEach((tab) => {
-      tab.classList.remove('active');
+    const isActive = selected.classList.toggle('active');
+
+    console.log(isActive);
+    const addPrice = +selected.dataset.tab;
+    if (isActive) {
+      totalPrice += addPrice;
+      additivesPrice += addPrice
+    } else {
+      totalPrice -= addPrice;
+      additivesPrice -= addPrice
+    }
+
+    createTotalPrice();
+  }
+
+  tabsAdditives.addEventListener('click', chooseAdditives);
+
+  const createTotalPrice = () => {
+    modalPriceContainer.innerHTML = '';
+ 
+    modalPriceContainer.insertAdjacentHTML('beforeend',
+      `<h3 class="font-h3">Total:</h3>
+    <h3 class="font-h3">$${totalPrice.toFixed(2)}</h3>`
+    );
+  }
+
+ 
+
+  const createSizeBlock = (obj) => {
+    const objKeys = Object.keys(obj.sizes);
+
+    objKeys.forEach((key) => {
+      const tabEl = document.createElement('button');
+      tabEl.innerHTML = `<span class="icon">${key.toUpperCase()}</span>${obj.sizes[key].size}`;
+    
+      tabEl.classList.add('tab-item');
+      tabEl.classList.add('font-link-and-button');
+      tabEl.setAttribute('data-tab', obj.sizes[key]['add-price']);
+
+      if (key === 's') {
+        tabEl.classList.add('active');
+      }
+      tabsSize.appendChild(tabEl)
+    })
+    totalPrice = +obj.price;
+    createTotalPrice();
+
+    const chooseSize = (e) => {
+      const allTabs = tabsSize.querySelectorAll('.tab-item');
+      const selected = e.target.closest('.tab-item');
+      if (!selected) return;
+
+      allTabs.forEach((tab) => {
+        tab.classList.remove('active');
       })
       
       selected.classList.add('active');
-    const addPrice = +selected.dataset.tab;
-    totalPrice = +obj.price + addPrice +additivesPrice;   
-    createTotalPrice();
-  }
-  tabsSize.addEventListener('click', chooseSize)  
-}
-
-const createAdditivesBlock = (obj) => {
-  obj.additives.forEach((add, i) => {
-    const tabEl = document.createElement('button');
-    tabEl.innerHTML = `<span class="icon">${i + 1}</span>${add.name}`;
-    
-    tabEl.classList.add('tab-item');
-    tabEl.classList.remove('active');
-    tabEl.classList.add('font-link-and-button');
-    tabEl.setAttribute('data-tab', add['add-price']);
-    
-    tabsAdditives.appendChild(tabEl);
-  }) 
-}
-
-const createSizeAndAdditivesBlocks = (clickedTitle) => {
-  products.forEach((obj) => {
-    if (obj.name === clickedTitle) {
-      createSizeBlock(obj)
-      createAdditivesBlock(obj)
+      const addPrice = +selected.dataset.tab;
+      totalPrice = +obj.price + addPrice + additivesPrice;
+      createTotalPrice();
     }
-  })
-}
+    tabsSize.addEventListener('click', chooseSize)
+  }
 
-
-const createModal = (clickedCard) => {
-  const imageEl = clickedCard.querySelector('.image-card');
-  const titleEl = clickedCard.querySelector('.title');
-  const descriptionEl = clickedCard.querySelector('.description');
- 
-  imageContainer.insertAdjacentHTML('beforeend',
-  `<img src="${imageEl.getAttribute('src')}"
-      alt="product">
-    `)   
+  const createAdditivesBlock = (obj) => {
+    obj.additives.forEach((add, i) => {
+      const tabEl = document.createElement('button');
+      tabEl.innerHTML = `<span class="icon">${i + 1}</span>${add.name}`;
     
-  titleContainer.insertAdjacentHTML('afterbegin',
-  `<h3 class="font-h3 modal-title">${titleEl.textContent}</h3>
+      tabEl.classList.add('tab-item');
+      tabEl.classList.remove('active');
+      tabEl.classList.add('font-link-and-button');
+      tabEl.setAttribute('data-tab', add['add-price']);
+    
+      tabsAdditives.appendChild(tabEl);
+    })
+  }
+
+  const createSizeAndAdditivesBlocks = (clickedTitle) => {
+    products.forEach((obj) => {
+      if (obj.name === clickedTitle) {
+        createSizeBlock(obj)
+        createAdditivesBlock(obj)
+      }
+    })
+  }
+
+
+  const createModal = (clickedCard) => {
+    const imageEl = clickedCard.querySelector('.image-card');
+    const titleEl = clickedCard.querySelector('.title');
+    const descriptionEl = clickedCard.querySelector('.description');
+ 
+    imageContainer.insertAdjacentHTML('beforeend',
+      `<img src="${imageEl.getAttribute('src')}"
+      alt="product">
+    `)
+    
+    titleContainer.insertAdjacentHTML('afterbegin',
+      `<h3 class="font-h3 modal-title">${titleEl.textContent}</h3>
   <p class="font-body-medium">${descriptionEl.textContent}</p>
-  `)   
+  `)
   
-  createSizeAndAdditivesBlocks(titleEl.textContent)
+    createSizeAndAdditivesBlocks(titleEl.textContent)
   
+  }
+  const clearModal = () => {
+    imageContainer.innerHTML = '';
+    titleContainer.innerHTML = '';
+    tabsAdditives.innerHTML = '';
+    tabsSize.innerHTML = '';
+  }
+
+  const closeModal = () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+    document.body.classList.remove('modal-open');
+    clearModal();
+  };
+
+  const openModal = (e) => {
+    const clickedCard = e.target.closest('.card-item');
+    if (!clickedCard) return;
+
+    createModal(clickedCard);
+
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+    document.body.classList.add('modal-open');
+  }
+
+
+  cardsContainer.addEventListener('click', openModal);
+  btnCloseModal.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
 }
-const clearModal = () => {
-  imageContainer.innerHTML = '';
-  titleContainer.innerHTML = '';
-  tabsAdditives.innerHTML = '';
-  tabsSize.innerHTML = '';
-}
-
-const closeModal = () => {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-  document.body.classList.remove('modal-open');
-  clearModal();
-};
-
-const openModal = (e) => {
-  const clickedCard = e.target.closest('.card-item');
-  if (!clickedCard) return;
-
-  createModal(clickedCard);
-
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-  document.body.classList.add('modal-open');
-}
-
-
-cardsContainer.addEventListener('click', openModal);
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+toggleModalWindow();
